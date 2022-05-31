a71ff471cfae2b4db06a6e8ec314418d22a0a893
gloomydumber <chromacrow@naver.com>
2022-05-31 10:46:26 +0900

    modded

--- problem02.scm
+++ problem02.scm
@@ -1,24 +1,16 @@
-; 이것을 기반으로 수정 필요
-
 (define (my-append lst item)
   (append lst (list item)))
 
 (define (takeElement n l)(list-ref l n))
 
 (define (takeTail n l)
-  (takeTailRec n l '()))
+  (takeTailRec 0 n l '()))
 
-(define (takeTailRec n l appendedL)
-  (if (< n 0)
+(define (takeTailRec k n l appendedL)
+  (if (>= k n)
       appendedL
-      (let ((m (- n 1)))
-        ;(append appendedL (list (takeElement n l))))))
-        (takeTailRec m l (append appendedL (list (takeElement n l)))))))
-      ;(takeElement n l))))
-     ;(takeTailRec m l (append appendedL (takeElement n l))))
+      (let ((m (+ k 1)))
+        (takeTailRec m n l (append appendedL (list (takeElement k l)))))))
 
 
 (takeTail 3 '(1 2 3 4 5))
-
-;(list-ref '(2 4 3 5) 0)
-;(takeElement 0 '(2 4 3 5))
